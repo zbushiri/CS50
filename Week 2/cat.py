de55afc38821dc0db6255028ad1fe1697b0f5f34
Zaki <bushirizaki@gmail.com>
2026-07-30 15:41:58 +0000

    Thu, Jul 30, 2026, 11:41 AM -04:00

--- Week 2/cat.py
+++ Week 2/cat.py
@@ -1,7 +1,16 @@
-while True:
-    n = int(input("What's n? "))
-    if n > 0:
-        break
+def main():
+    number = get_number()
+    meow(3)
 
-for _ in range(n):
-    print("meow")
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+main()
